023fe03b12323cea433e851d08e26b17be3357eb
Michaela Meissner <m.p.meissner85@gmail.com>
2023-11-12 10:24:39 -0500

    Added Products Ssection

--- whiskers/src/App.js
+++ whiskers/src/App.js
@@ -17,12 +17,7 @@ function App() {
       </div>
       <div class="container">
         <div>
-          <img
-            src="./Components/CatCafe2.jpg"
-            alt="cat on counter with coffee"
-            height="425px"
-            width="1000px"
-          />
+          <img src="./Components/CatCafe2.jpg" alt="cat on counter with coffee" height="425px" width="1000px"/>
           <div class="title">
             <h1 id="cafe-name">Frosted Whiskers</h1>
             <h2>Cat Café and Pâtisserie</h2>
@@ -47,22 +42,17 @@ function App() {
           </p>
         </div>
         <div class="menu">
-        <h3 id="menu">Menu</h3>
-        <hr/>
-        <p>Are you craving a delicious coffee?</p>
-        <p>Want a homemade pastry without the mess?</p>
-        <p>Then come on down to Old Town Winchester!</p>
-        <p>The delicious joy of caffeine, sweets, and playing with cats.</p>
-        <p>There's nothing sweeter! <img src="./Components/Cat_in_cup.jpg" alt="cat in a cup" height= "50px" width="50px" /></p>
-        <br/>
+          <h3 id="menu">Menu</h3>
+          <hr/>
+            <p>Are you craving a delicious coffee?</p>
+            <p>Want a homemade pastry without the mess?</p>
+            <p>Then come on down to Old Town Winchester!</p>
+            <p>The delicious joy of caffeine, sweets, and playing with cats.</p>
+            <p>There's nothing sweeter! <img src="./Components/Cat_in_cup.jpg" alt="cat in a cup" height= "50px" width="50px" /></p>
+          <br/>
         <div id="menu-whole">
           <div class="coffee">
-            <img
-              src="./Components/Coffee-background.jpg"
-              alt="coffe bean background"
-              height="600px"
-              width="550px"
-            />
+            <img src="./Components/Coffee-background.jpg" alt="coffe bean background" height="600px" width="550px"/>
             <div class="items">
               <ul><strong>Abyssianian</strong> -- Coffee and 3 shots espresso</ul>
               <ul><strong>Persian</strong> -- Espresso and steamed milk</ul>
@@ -83,7 +73,87 @@ function App() {
             </div>
           </div>
         </div>
+        </div>
+        <br />
+      <hr />
+      <div class="products">
+        <h3 id="products">Products</h3>
+        <hr />
+        <p>Want to visit with our wonderful and adoptable kitties?</p>
+        <p>
+          Here's a small example of the toys availble for use. All toys can be
+          purchased in-house or through
+          <a href="https://www.christhecat.com/">ChrisTheCat</a>
+        </p>
+        <p>
+          All proceeds go to local rescues. <a href="#local_rescues">See below</a> for
+          those we work with
+        </p>
+        <div class="grid-container">
+          <div class="grid-item">
+            <img
+              src="./assets/BumbleBee.jpg"
+              alt="bumblee cat toy"
+              id="cat-toy"
+              height="250px"
+              width="250px"
+            />
+            <h4>Bumblee Bee</h4>
+          </div>
+          <div class="grid-item">
+            <img
+              src="./assets/Dragonfly.jpg"
+              alt="dragonfly cat toy"
+              id="cat-toy"
+              height="250px"
+              width="250px"
+            />
+            <h4>Dragon Fly</h4>
+          </div>
+          <div class="grid-item">
+            <img
+              src="./assets/LightningRod.jpg"
+              alt="lightning rod cat toy"
+              id="cat-toy"
+              height="250px"
+              width="250px"
+            />
+            <h4>Ligtning Rod</h4>
+          </div>
+          <div class="grid-item">
+            <img
+              src="./assets/PolychromePom.jpg"
+              alt="polychrome cat toy"
+              id="cat-toy"
+              height="250px"
+              width="250px"
+            />
+            <h4>Polychrome Pom</h4>
+          </div>
+          <div class="grid-item">
+            <img
+              src="./assets/PuffCatty.jpg"
+              alt="feather cat toy"
+              id="cat-toy"
+              height="250px"
+              width="250px"
+            />
+            <h4>Puffy Cat</h4>
+          </div>
+          <div class="grid-item">
+            <img
+              src="./assets/SummerShrimp.jpg"
+              alt="colorful shrimp cat toy"
+              id="cat-toy"
+              height="250px"
+              width="250px"
+            />
+            <h4>Summer Shrimp</h4>
+          </div>
+        </div>
       </div>
+      <br />
+      <hr />
       </div>
     </div>
   );
